431feb761bf946df3555ffe975f4b6e8ba51fb50
zakaev <zhumgal.akaev@zensoft.io>
2019-07-02 18:16:53 +0600

    implement user service

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -4,7 +4,11 @@ import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRootAsync({
+    useFactory: () => ({
+      uri: 'mongodb://localhost/nest',
+    }),
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/components/user/user.service.ts
+++ src/components/user/user.service.ts
@@ -6,9 +6,13 @@ import User from '../../models/user.model'
 export class UserService {
   constructor(@Inject('USER_MODEL') private readonly userModel: Model<User.UserDocument>) {}
 
-  async create(user: User.CreateDto) {
+  async create(user: User.CreateDto): Promise<User.UserDocument> {
     const result = new this.userModel(user)
     return await result.save()
+  }
+
+  async findAll(): Promise<User.UserDocument[]> {
+    return await this.userModel.find().exec()
   }
 
 }
